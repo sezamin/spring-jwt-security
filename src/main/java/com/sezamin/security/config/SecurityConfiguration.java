18b7af9e05de3fd941771ad00e614184366fc467
Egamberdi Shukurov <shukurove@gmail.com>
2023-01-04 14:30:02 +0500

    Jwt Security Changes

--- src/main/java/com/sezamin/security/config/SecurityConfiguration.java
+++ src/main/java/com/sezamin/security/config/SecurityConfiguration.java
@@ -20,20 +20,15 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
-        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.csrf().disable();
+
+        http.authorizeHttpRequests().requestMatchers("/api/v1/auth/**").permitAll();
+        http.authorizeHttpRequests().anyRequest().authenticated();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authenticationProvider(authenticationProvider);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
